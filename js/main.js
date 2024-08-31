2150a8aa6217fbcc6cb21926ca9925231e980eae
Jacek Szulc <j.szulc77@wp.pl>
2024-08-31 15:41:52 +0200

    Projekt#7.2

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ let $todoInput; // miejsce, gdzie użytkownik wpisuje treść zadania
 let $alertInfo; // info o braku zadań / konieczności dodania tekstu
 let $addBtn; // przycisk ADD - dodaje nowe elementy do listy
 let $ulList; // nasza lista zadań, tagi <ul></ul>
+let $newTask; // nowo dodany LI, nowe zadanie
 
 const main = () => {
 	prepareDOMElements();
@@ -17,6 +18,21 @@ const prepareDOMElements = () => {
 };
 
 // nadajemy nasłuchiwanie
-const prepareDOMEvents = () => {};
+const prepareDOMEvents = () => {
+	$addBtn.addEventListener("click", addNewTask);
+};
+
+const addNewTask = () => {
+	if ($todoInput.value !== "") {
+		$newTask = document.createElement("li");
+		$newTask.innerText = $todoInput.value;
+		$ulList.appendChild($newTask);
+
+		$todoInput.value = "";
+		$alertInfo.innerText = "";
+	} else {
+		$alertInfo.innerText = "Wpisz treść zadania!";
+	}
+};
 
 document.addEventListener("DOMContentLoaded", main);
