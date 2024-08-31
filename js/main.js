e565e87a9e69ed08e3949db3a1f5a58c71f044f5
Jacek Szulc <j.szulc77@wp.pl>
2024-08-31 17:45:44 +0200

    Projekt#7.6

--- js/main.js
+++ js/main.js
@@ -3,6 +3,12 @@ let $alertInfo; // info o braku zadań / konieczności dodania tekstu
 let $addBtn; // przycisk ADD - dodaje nowe elementy do listy
 let $ulList; // nasza lista zadań, tagi <ul></ul>
 let $newTask; // nowo dodany LI, nowe zadanie
+let $popup; // pobrany popup
+let $popupInfo; // alert w popup'ie, jak się doda pusty tekst
+let $editedTodo; // edytowany Todo
+let $popupInput; // tekst wpisywany w inputa w popup'ie
+let $addPopupBtn; // przycisk "zatwierdź" w popup'ie
+let $closeTodoBtn; // przycisk od zamykania popup'a
 
 const main = () => {
 	prepareDOMElements();
@@ -15,12 +21,18 @@ const prepareDOMElements = () => {
 	$alertInfo = document.querySelector(".alertInfo");
 	$addBtn = document.querySelector(".addBtn");
 	$ulList = document.querySelector(".todoList ul");
+	$popup = document.querySelector(".popup");
+	$popupInfo = document.querySelector(".popupInfo");
+	$popupInput = document.querySelector(".popupInput");
+	$addPopupBtn = document.querySelector(".accept");
+	$closeTodoBtn = document.querySelector(".cancel");
 };
 
 // nadajemy nasłuchiwanie
 const prepareDOMEvents = () => {
 	$addBtn.addEventListener("click", addNewTask);
-    $ulList.addEventListener("click", checkClick)
+	$ulList.addEventListener("click", checkClick);
+	$closeTodoBtn.addEventListener("click", closePopup);
 };
 
 const addNewTask = () => {
@@ -64,10 +76,18 @@ const checkClick = e => {
 		e.target.closest("li").classList.toggle("completed");
 		e.target.closest("button").classList.toggle("completed");
 	} else if (e.target.closest("button").className === "edit") {
-		console.log("edit");
+		editTask();
 	} else if (e.target.closest("button").className === "delete") {
 		console.log("delete");
 	}
+};
+
+const editTask = () => {
+	$popup.style.display = "flex";
+};
+
+const closePopup = () => {
+	$popup.style.display = "none";
 };
 
 document.addEventListener("DOMContentLoaded", main);
